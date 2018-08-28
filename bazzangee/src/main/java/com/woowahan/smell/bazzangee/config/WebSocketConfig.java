3f058fa7f0a73526b4910dda2c4be9ed9e751441
권현아 <gusdk7656@naver.com>
2018-08-28 20:24:19 +0900

    [kwonhyeona] 부러워요 알림 기능 구현했습니다. (#94)
    * 부러워요 기능 프론트 구현
    
    * 부러워요 기능 구현
    
    - 테스트를 위해 세션에 유저를 넣어주는BasicAuthInterceptor 추가
    - 부러워요 서버 기능 구현
    > /api/reviews/{id}/good api 추가
    > good repository 추가
    - 부러워요 프론트엔드 기능 구현
    > css 수정
    > 해당 api 로 요청보내는 ajax 구현
    > html 을 생성해주는 htmlGenerator 구현
    
    * 채팅 프로토타입 코드 작성
    
    * 채팅 기본 기능 구현
    
    * 채팅 기능 프로토타입 구현
    
    - 프론트엔드 채팅 기능 구현
    > websocket 과 stomp 프레임워크를 연동
    > socketManager.js 를 추가해 소켓을 관리하는 클래스 생성
    > chat.js 를 추가해 메인화면에서 채팅 기능을 관리하는 클래스 생성
    > index.html 에 채팅 관련 코드 추가
    > chat.css 에 채팅 관련된 css 추가
    
    - 백엔드 채팅 기능 구현
    > websocket 을 이용하기 위해 gradle 추가
    > WebSocketConfig 을 추가해 소켓 관련된 설정 추가
    > 소켓에서 사용할 dto 추가
    > HttpHandshakeInterceptor 를 추가해 소켓 연결 시 세션 설정
    > ChatController 를 추가해 소켓 메세지에 대한 컨트롤러 구현
    
    * 채팅 기능 프로토타입 구현
    
    - 프론트엔드 채팅 기능 구현
    > websocket 과 stomp 프레임워크를 연동
    > socketManager.js 를 추가해 소켓을 관리하는 클래스 생성
    > chat.js 를 추가해 메인화면에서 채팅 기능을 관리하는 클래스 생성
    > index.html 에 채팅 관련 코드 추가
    > chat.css 에 채팅 관련된 css 추가
    
    - 백엔드 채팅 기능 구현
    > websocket 을 이용하기 위해 gradle 추가
    > WebSocketConfig 을 추가해 소켓 관련된 설정 추가
    > 소켓에서 사용할 dto 추가
    > HttpHandshakeInterceptor 를 추가해 소켓 연결 시 세션 설정
    > ChatController 를 추가해 소켓 메세지에 대한 컨트롤러 구현
    
    * appspec 버전 수정
    
    * 알림기능 구현
    
    * 불필요한 테스트 삭제

--- bazzangee/src/main/java/com/woowahan/smell/bazzangee/config/WebSocketConfig.java
+++ bazzangee/src/main/java/com/woowahan/smell/bazzangee/config/WebSocketConfig.java
@@ -1,14 +1,18 @@
 package com.woowahan.smell.bazzangee.config;
 
 import com.woowahan.smell.bazzangee.interceptor.HttpHandshakeInterceptor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import java.util.Arrays;
+
 import static com.woowahan.smell.bazzangee.config.ChatRoomName.*;
 
+@Slf4j
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
@@ -20,17 +24,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-//        registry.addEndpoint(GENERAL.getRoomName())
-//                .setHandshakeHandler(new SocketHandshakeHandler())
-//                .withSockJS();
-        registry.addEndpoint(GENERAL.getRoomName()).addInterceptors(new HttpHandshakeInterceptor()).withSockJS();
-        registry.addEndpoint(CHICKEN.getRoomName()).addInterceptors(new HttpHandshakeInterceptor()).withSockJS();
-        registry.addEndpoint(PIZZA.getRoomName()).addInterceptors(new HttpHandshakeInterceptor()).withSockJS();
-        registry.addEndpoint(WESTERN_FOOD.getRoomName()).addInterceptors(new HttpHandshakeInterceptor()).withSockJS();
-        registry.addEndpoint(KOREAN_FOOD.getRoomName()).addInterceptors(new HttpHandshakeInterceptor()).withSockJS();
-        registry.addEndpoint(CHINESE_FOOD.getRoomName()).addInterceptors(new HttpHandshakeInterceptor()).withSockJS();
-        registry.addEndpoint(JAPANESE_FOOD.getRoomName()).addInterceptors(new HttpHandshakeInterceptor()).withSockJS();
-        registry.addEndpoint(SNACK_BAR.getRoomName()).addInterceptors(new HttpHandshakeInterceptor()).withSockJS();
-        registry.addEndpoint(HAMBURGER.getRoomName()).addInterceptors(new HttpHandshakeInterceptor()).withSockJS();
+        Arrays.stream(ChatRoomName.values()).forEach((chatRoom) ->
+                registry.addEndpoint(chatRoom.getRoomName())
+                        .addInterceptors(new HttpHandshakeInterceptor())
+                        .withSockJS());
     }
 }
--- bazzangee/src/test/java/com/woowahan/smell/bazzangee/web/api/ApiReviewControllerTest.java
+++ bazzangee/src/test/java/com/woowahan/smell/bazzangee/web/api/ApiReviewControllerTest.java
@@ -1,28 +1,20 @@
 package com.woowahan.smell.bazzangee.web.api;
 
-import com.amazonaws.services.s3.AmazonS3Client;
 import com.woowahan.smell.bazzangee.aws.S3Uploader;
 import com.woowahan.smell.bazzangee.domain.User;
-import com.woowahan.smell.bazzangee.dto.ReviewRequestDto;
-import com.woowahan.smell.bazzangee.dto.ReviewResponseDto;
-import com.woowahan.smell.bazzangee.exception.UnAuthenticationException;
+import com.woowahan.smell.bazzangee.dto.request.ReviewRequestDto;
+import com.woowahan.smell.bazzangee.dto.response.ReviewResponseDto;
 import com.woowahan.smell.bazzangee.repository.ReviewRepository;
-import com.woowahan.smell.bazzangee.service.MockReviewService;
-import com.woowahan.smell.bazzangee.service.MockS3Uploader;
 import com.woowahan.smell.bazzangee.service.ReviewService;
 import com.woowahan.smell.bazzangee.utils.HttpSessionUtils;
-import com.woowahan.smell.bazzangee.web.HtmlFormDataBuilder;
-import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.*;
 import org.springframework.mock.web.MockHttpSession;
-import org.springframework.util.MultiValueMap;
 import support.test.AcceptanceTest;
 
 import java.io.IOException;
@@ -57,18 +49,5 @@ public class ApiReviewControllerTest extends AcceptanceTest {
 
         assertThat(HttpStatus.OK).isEqualTo(apiReviewController.create(reviewRequestDto, session).getStatusCode());
         log.info("리뷰_등록 결과 : {} ", apiReviewController.create(reviewRequestDto, session).getBody());
-    }
-
-    @Test
-    public void 리뷰_좋아요() {
-        ResponseEntity<ReviewResponseDto> response = basicAuthTemplate().getForEntity(String.format("/api/reviews/%d/good", 1), ReviewResponseDto.class);
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-    }
-
-    @Test
-    public void 리뷰_좋아요_재시도() {
-        ResponseEntity<ReviewResponseDto> firstResponse = basicAuthTemplate().getForEntity(String.format("/api/reviews/%d/good", 1), ReviewResponseDto.class);
-        ResponseEntity<ReviewResponseDto> secondResponse = basicAuthTemplate().getForEntity(String.format("/api/reviews/%d/good", 1), ReviewResponseDto.class);
-        assertThat(secondResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 }
