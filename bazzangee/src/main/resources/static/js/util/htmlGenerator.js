6ae54c16c4117f56dfa12711580789a724525572
우아테크캠프_15 <woowatechcamp_15@woowatechcamp15.local>
2018-08-29 21:01:44 +0900

    옷장 CSS에 주문내역 추가 및 디자인 변경
    - 옷장에 주문내역 추가
    > 주문내역을 타임라인처럼 구현
    > 추후 리뷰 쓰기 및 삭제 등 연동 필요
    > 옷장 페이지 양 옆에 타임라인과 주문내역을 나타내는 이미지 추가

--- bazzangee/src/main/resources/static/js/util/htmlGenerator.js
+++ bazzangee/src/main/resources/static/js/util/htmlGenerator.js
@@ -1,4 +1,8 @@
+//const iconPath = '/img/category_image_no_name/';
+//const iconImages = ['chicken.png', 'pizza.png', 'hansik.png', 'jungsik.png', 'jokbal.png', 'ilsik.png', 'yasik.png']
 const HtmlGenerator = (function () {
+    const iconPath = '/img/category_image_no_name/';
+    const iconImages = ['chicken.png', 'pizza.png', 'hansik.png', 'jungsik.png', 'jokbal.png', 'ilsik.png', 'yasik.png']
     return {
         getReviewHTML(reviewDto) {
             let written = reviewDto.review.writtenTime;
@@ -124,6 +128,95 @@ const HtmlGenerator = (function () {
                     </div>
                 </li>`;
             }
+            return orderFoodHTML;
+        },
+        getOrderFoodWithReviewHTML(orderFood) {
+            let orderTime = orderFood.orderTime;
+            let orderedDate = orderTime.slice(0, 10);
+            let orderedTime = orderTime.slice(11, 16);
+            let orderFoodHTML = ``;
+            // 리뷰가 있는 경우
+            const review = orderFood.review;
+            let contents = orderFood.review.contents.replace(/(?:\r\n|\r|\n)/g, '<br/>');
+            orderFoodHTML = `<li class="orderfood-li" data-id="${orderFood.id}">
+                    <time class="cbp_tmtime" datetime="${orderFood.orderTime}"><span>${orderedDate}</span> <span>${orderedTime}</span></time>
+                    <div class="cbp_tmicon cbp_tmicon-phone"></div>
+                    <div class="cbp_tmlabel" style="display: flex">
+                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                            <img src="${review.imageUrl}" width="100%">
+                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="padding: 0px; margin-top: 10px">
+                                <button class="btn btn-warning width100 good-btn" style="text-align: center" data-value="${review.id}" >
+                                    <span style="color: whitesmoke; font-size: 1vw" data-value="${review.id}"><i class="fa fa-thumbs-up" data-value="${review.id}"></i>부러워요</span>
+                                    <span style="color: #6b0f24; font-size: 1.2vw" data-value="${review.id}">${review.goods.length}</span>
+                                </button>
+                            </div>
+                        </div>
+                        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8 card-contents" style="padding: 0px">
+                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <h4>${orderFood.food.name}</h4>
+                            </div>
+                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                <div class="rate" data-rate-value="${review.starPoint}" style="pointer-events:none;"></div>
+                                <p>${contents}</p>
+                                <p class="t-right">
+                                    <a href="#" style="font-weight: 600; color: aliceblue">
+                                        <i class="fa fa-map-marker"> ${orderFood.food.restaurant.name}</i>
+                                    </a>
+                                    <button class="statistics" value="${orderFood.food.restaurant.id}"></button>
+                                </p>
+                                <div class="update-delete-btn">
+                                    <input type="button" class="btn-review-update-form btn btn-danger" value="수정">
+                                    <input type="button" class="btn-review-delete btn btn-danger" value="삭제">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>`;
+            return orderFoodHTML;
+        },
+        getOrderFoodWithoutReviewHTML(orderFood) {
+            let defaultImageURL = iconPath + iconImages[orderFood.food.restaurant.foodCategory.id - 1]
+            let orderTime = orderFood.orderTime;
+            let orderedDate = orderTime.slice(0, 10);
+            let orderedTime = orderTime.slice(11, 16);
+            let orderFoodHTML = ``;
+            // 리뷰 없는경우
+            orderFoodHTML = `<li class="orderfood-li" data-id="${orderFood.id}">
+                <time class="cbp_tmtime" datetime="${orderFood.orderTime}"><span>${orderedDate}</span> <span>${orderedTime}</span></time>
+                <div class="cbp_tmicon cbp_tmicon-phone"></div>
+                <div class="cbp_tmlabel" style="display: flex">
+                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                        <img src="${defaultImageURL}" width="100%">
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="padding: 0px; margin-top: 10px">
+                        </div>
+                    </div>
+                    <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8" style="padding: 0px">
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <h4>${orderFood.food.name}</h4>
+                        </div>
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <button type="button" class="orderfood-review-write">리뷰 쓰기</button>
+                            <div class="card card-position invisible">
+                                <img class="card-img-top" src="" style="width:200px">
+                                <button type="button" class="btn btn-danger btn-remove-image invisible">X</button>
+                                <div class="card-body">
+                                    <textarea rows="4" cols="50" name="contents" class="card-text"></textarea>
+                                    <input type="file" name="image" class="btn btn-primary" accept="image/*">
+                                    <div class="rate" data-rate-value="0"></div>
+                                    <input type="button" class="btn-review-submit btn btn-danger" value="저장">
+                                    <input type="button" class="btn-review-submit-cancel btn btn-danger" value="취소">
+                                </div>
+                            </div>
+                            <p class="t-right" style="margin-top:10px">
+                                <a href="#" style="font-weight: 600; color: aliceblue">
+                                    <i class="fa fa-map-marker"> ${orderFood.food.restaurant.name}</i>
+                                </a>
+                                <button class="statistics" value="${orderFood.food.restaurant.id}"></button>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </li>`;
             return orderFoodHTML;
         },
 
