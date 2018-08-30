99d28a70399de4650ab46df108d8efda48198dc0
우아테크캠프_2 <woowatechcamp_2@woowatechcamp2.local>
2018-08-30 21:12:49 +0900

    aws.yml 제거

--- bazzangee/src/main/java/com/woowahan/smell/bazzangee/BazzangeeApplication.java
+++ bazzangee/src/main/java/com/woowahan/smell/bazzangee/BazzangeeApplication.java
@@ -8,9 +8,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class BazzangeeApplication {
 
-    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+    private static final String APPLICATION_LOCATIONS = "spring.config.location="
             + "classpath:application.yml,"
-            + "classpath:aws.yml,"
             + "/home/ubuntu/Project/travis/db-application.yml";
 
     public static void main(String[] args) {
